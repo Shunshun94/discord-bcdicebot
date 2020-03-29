e6e3fbe9d28ac8fdd27daad5b99314008431f967
Shunshun94 <shunshun9460@gmail.com>
2020-03-29 17:17:21 +0900

    remove unnecessary imports

--- src/main/java/com/hiyoko/discord/bot/BCDice/DiceClient/BCDiceClient.java
+++ src/main/java/com/hiyoko/discord/bot/BCDice/DiceClient/BCDiceClient.java
@@ -7,8 +7,6 @@ import com.hiyoko.discord.bot.BCDice.dto.VersionInfo;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.net.URLEncoder;
