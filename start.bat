9f86d633bd37e7337a218c9fc98b678e9b91cca9
Shunshun94 <shunshun9460@gmail.com>
2020-05-31 23:34:21 +0900

    version info

--- start.bat
+++ start.bat
@@ -5,6 +5,8 @@ set DISCORD_BOT_TOKEN=
 set BCDICE_API_URL=
 set IGNORE_ERROR=
 REM set BCDICE_PASSWORD=PleaseChangeMeIfYouUseThis
+REM set BCDICE_API_SECONDARY=http://secondary.bcdice-api.yourdomain.co.jp/
+REM set BCDICE_DEFAULT_SYSTEM=DiceBot
 
 java -jar discord-bcdicebot.jar %DISCORD_BOT_TOKEN% %BCDICE_API_URL% %IGNORE_ERROR%
 pause
--- start.sh
+++ start.sh
@@ -4,5 +4,7 @@ DISCORD_BOT_TOKEN=
 BCDICE_API_URL=
 IGNORE_ERROR=
 # BCDICE_PASSWORD=PleaseChangeMeIfYouUseThis
+# BCDICE_API_SECONDARY=http://secondary.bcdice-api.yourdomain.co.jp/
+# BCDICE_DEFAULT_SYSTEM=DiceBot
 
 java -jar discord-bcdicebot.jar "$DISCORD_BOT_TOKEN" "$BCDICE_API_URL" "$IGNORE_ERROR"
