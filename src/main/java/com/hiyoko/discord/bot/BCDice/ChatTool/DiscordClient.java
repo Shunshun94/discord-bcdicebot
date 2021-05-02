16a4f99d7d6909f139c03b5b4f991f0ed653060c
Shunshun94 <shunshun9460@gmail.com>
2021-05-03 03:21:19 +0900

    start to develop

--- src/main/java/com/hiyoko/discord/bot/BCDice/ChatTool/DiscordClient.java
+++ src/main/java/com/hiyoko/discord/bot/BCDice/ChatTool/DiscordClient.java
@@ -47,6 +47,10 @@ public class DiscordClient implements ChatToolClient {
 		return result;
 	}
 
+	public String formatMessage(String input) {
+		
+	}
+
 	private List<String> getRoomIds() {
 		return api.getChannels().stream().filter(channel->{
 			return channel.getType().isTextChannelType();
@@ -73,5 +77,5 @@ public class DiscordClient implements ChatToolClient {
 			System.out.println("Discord Access Password is written in environment variable BCDICE_PASSWORD");
 			return env;
 		}
-	}	
+	}
 }
