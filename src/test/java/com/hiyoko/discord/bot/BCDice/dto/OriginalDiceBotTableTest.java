b093658d518bda2f0441055fed44861d81f9e328
Shunshun94 <shunshun9460@gmail.com>
2021-05-04 14:17:21 +0900

    add lacked invalid dice bot - issue #96

--- src/test/java/com/hiyoko/discord/bot/BCDice/dto/OriginalDiceBotTableTest.java
+++ src/test/java/com/hiyoko/discord/bot/BCDice/dto/OriginalDiceBotTableTest.java
@@ -12,7 +12,7 @@ public class OriginalDiceBotTableTest extends TestCase {
 		OriginalDiceBotClient client = new OriginalDiceBotClient("./testDiceBots");
 		OriginalDiceBotTable a = client.getDiceBot("サンプルダイスボット-ラーメン表");
 		assertFalse(a.isValid);
-		OriginalDiceBotTable b = client.getDiceBot("ダブルクロス-トライブリード");
+		OriginalDiceBotTable b = client.getDiceBot("サンプルダイスボット-長過ぎる表");
 		assertFalse(b.isValid);
 		OriginalDiceBotTable c = client.getDiceBot("サンプルダイスボット-夜食表");
 		assertTrue(c.isValid);
