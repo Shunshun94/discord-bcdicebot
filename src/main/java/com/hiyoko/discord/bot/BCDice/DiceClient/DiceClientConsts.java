86e540caa8cbffc5d1b71fefad876409625c7c47
Shunshun94 <shunshun9460@gmail.com>
2021-07-04 16:34:50 +0900

    fix for v2.3.0

---
+++ src/main/java/com/hiyoko/discord/bot/BCDice/DiceClient/DiceClientConsts.java
@@ -0,0 +1,7 @@
+package com.hiyoko.discord.bot.BCDice.DiceClient;
+
+public class DiceClientConsts {
+	public static int OK = 200;
+	public static int BAD_REQUEST = 400;
+	public static int REQUEST_URI_TOO_LONG = 414;  
+}
